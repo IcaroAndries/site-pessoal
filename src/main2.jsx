e267df535ed76681c0766ae9db5b679ded3c7c44
Icaro Andries <icaro.andries15@gmail.com>
2023-06-20 01:50:57 -0300

    testando netlify

---
+++ src/main2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Princesa } from './pages';
+import './index.sass'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Princesa />
+  </React.StrictMode>,
+)
